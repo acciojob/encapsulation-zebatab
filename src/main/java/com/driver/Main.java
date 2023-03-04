18e52d4c60675cd46ef7e1346885a32560d06773
zebatab <zebatab094@gmail.com>
2023-03-04 21:42:48 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Kunal";
+        obj.setName("Kunal");
+        obj.getName();
+    }
 }
